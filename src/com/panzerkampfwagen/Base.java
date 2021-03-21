7fbbfdb7daf27aa586e5ef4ac20201c4d55c7109
Gergely Dremák <gergely089@gmail.com>
2021-03-21 23:39:51 +0100

    <| Base cost

--- src/com/panzerkampfwagen/Base.java
+++ src/com/panzerkampfwagen/Base.java
@@ -23,6 +23,7 @@ public class Base implements BuildableItem {
 	@Override
 	public Item[] getBuildCost() {
 		System.out.println("Base.getBuildCost");
-		return new Item[] { new Struts(), new Struts(), new Struts(), new Struts() };
+		return new Item[] { new Coal(), new Coal(), new Coal(), new Ice(), new Ice(), new Ice(), new Iron(), new Iron(),
+				new Iron(), new Uranium(), new Uranium(), new Uranium() };
 	}
 }
