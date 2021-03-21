c11c41cc197eb326c508bc0c8cd00614903ba031
Gergely Dremák <gergely089@gmail.com>
2021-03-22 00:17:14 +0100

    tryBuild start

--- src/com/panzerkampfwagen/Bill.java
+++ src/com/panzerkampfwagen/Bill.java
@@ -4,23 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Bill {
+	class OwnerRecord {
+		public OwnerRecord(Item item, Settler owner) {
+			this.item = item;
+			this.owner = owner;
+		}
+
+		Item item;
+		Settler owner;
+	}
+
 	BuildableItem result;
+	private List<Item> need = new ArrayList<>();
+	private List<OwnerRecord> have = new ArrayList<>();
 
 	public Bill(BuildableItem result) {
 		this.result = result;
 		this.need.addAll(List.of(result.getBuildCost()));
 	}
 
-	List<Item> need = new ArrayList<>();
-
-	List<OwnerRecord> have = new ArrayList<>();
-
 	public void tryBuild(Settler s) {
-		s.getInventory();
-	}
-
-	class OwnerRecord {
-		Item item;
-		Settler owner;
+		List<Item> inv = s.getInventory();
+		for (Item neededItem : need) {
+			for (int i = 0; i < inv.size(); i++) {
+				Item currentItem = inv.get(i);
+				if (currentItem.sameAs(neededItem)) {
+					inv.remove(currentItem);
+					need.remove(neededItem);
+				}
+			}
+		}
 	}
 }
