5a8468023e45f4540149c1e7b4212fd8c5ea0387
Gergely Dremák <gergely089@gmail.com>
2021-04-20 23:28:31 +0200

    Player CLI

--- src/com/panzerkampfwagen/controllers/Player.java
+++ src/com/panzerkampfwagen/controllers/Player.java
@@ -1,15 +1,26 @@
 package com.panzerkampfwagen.controllers;
 
-import java.util.List;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 import com.panzerkampfwagen.Utils;
 import com.panzerkampfwagen.units.Settler;
 
 public class Player extends Controller {
+	protected Map<String, Command> commands = new HashMap<>();
 	protected String name;
 
 	public Player(String name) {
 		this.name = name;
+		commands.put("status", this::status);
+		commands.put("move", this::move);
+		commands.put("toggle_hide", this::toggleHide);
+		commands.put("mine", this::mine);
+		commands.put("drop", this::drop);
+		commands.put("build", this::build);
+		commands.put("list", this::list);
+		commands.put("end", this::end);
 	}
 
 	private void cliPrompt() {
@@ -17,7 +28,7 @@ public class Player extends Controller {
 	}
 
 	public boolean prompt(String question) {
-		System.out.print(question + " (y/anything_else)");
+		System.out.println(question + " (y/anything_else)");
 		cliPrompt();
 		String answer = Utils.scanner.nextLine();
 		return answer.equals("y");
@@ -26,11 +37,20 @@ public class Player extends Controller {
 	@Override
 	public void takeTurn() {
 		super.takeTurn();
-		String answer;
+		String answer[];
 		do {
 			cliPrompt();
-			answer = Utils.scanner.nextLine();
-
+			answer = Utils.scanner.nextLine().split(" ");
+			Command c = commands.get(answer[0]);
+			if (c == null) {
+				System.out.println("Ismeretlen parancs.");
+				continue;
+			}
+			if (answer.length > 1) {
+				c.execute(Arrays.copyOfRange(answer, 1, answer.length));
+				continue;
+			}
+			c.execute();
 		} while (this.steps > 0);
 	}
 
@@ -42,19 +62,16 @@ public class Player extends Controller {
 	// #region commands
 	@FunctionalInterface
 	interface Command {
-		void execute(String... params);
+		public void execute(String... params);
 	}
-	protected Command commands[] = new Command[] {
-		this::status,
-		this::move,
-		this::toggleHide, mine, drop, build, list
-	};
+
 	private void status(String... params) {
 		System.out.println(unit.status());
 	}
 
 	private void move(String... params) {
-		if(params.length == 0) return;
+		if (params.length == 0)
+			return;
 		int index = Integer.parseInt(params[0]);
 		unit.move(unit.getReceiver().getNeighbour(index));
 	}
@@ -68,29 +85,35 @@ public class Player extends Controller {
 	}
 
 	private void drop(String... params) {
-		if(params.length == 0) return;
+		if (params.length == 0)
+			return;
 		int index = Integer.parseInt(params[0]);
 		((Settler) unit).dropCargo(index);
 	}
 
 	private void build(String... params) {
-		if(params.length == 0) return;
-		((Settler)unit).build(params[0]);
+		if (params.length == 0)
+			return;
+		((Settler) unit).build(params[0]);
 	}
 
-	private void list(String... params){
+	private void end(String... params) {
+		this.steps = 0;
+	}
+
+	private void list(String... params) {
+		if (params.length == 0)
+			return;
 		switch (params[0]) {
-			case "inventory":
-				System.out.println(Utils.joinList(((Settler) unit).getInventory()));
-				return;
-
-			case "gate_inventory":
-				System.out.println(Utils.joinList(((Settler) unit).getGateInventory()));
-				return;
-
-			case "destinations":
-				System.out.println(Utils.joinList(List.of(((Settler) unit).getReceiver().getNeighbours())));
-				return;
+		case "inventory":
+			System.out.println(Utils.joinList(((Settler) unit).getInventory()));
+			return;
+		case "gate_inventory":
+			System.out.println(Utils.joinList(((Settler) unit).getGateInventory()));
+			return;
+		case "destinations":
+			System.out.println(Utils.joinList(((Settler) unit).getReceiver().getNeighbours()));
+			return;
 		}
 	}
 	// #endregion
